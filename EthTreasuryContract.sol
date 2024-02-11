d322f395c369d38bddb6b1e0ca884f81c915b7f6
logan1208 <62975766+logan1208@users.noreply.github.com>
2024-02-12 00:06:04 +0900

    initial commit

---
+++ EthTreasuryContract.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./IBlast.sol";
+import "./IERC20.sol";
+
+
+contract EthTreasuryContract {
+
+    //TOR token balances
+    mapping(address account => uint256) private _balances;
+    //Delegate token
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    uint256 private _totalTorBalance=0;    
+    uint256 private _totalEthBalance=0;    
+    uint256 public _exchangeRate;
+    uint8 public _depositFeeRate;
+    uint8 public _withdrawFeeRate;
+    uint8 private _decimal=18;
+    address private _owner;    
+    IERC20 public _token;
+    IBlast public blastToken;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+        // Event emitted when a swap occurs
+    event Swap(address indexed sender, uint256 amountIn, uint256 amountOut);
+    event Withdrawal(address indexed sender, uint256 amountIn, uint256 amountOut);
+
+
+    modifier onlyOwner(){
+        require(msg.sender == _owner, "You are not the owner");
+        _;
+    }
+
+    constructor(address token) {
+        _token = IERC20(token);
+        _owner = msg.sender;
+        _exchangeRate = 5000;
+        _depositFeeRate = 1;
+        _withdrawFeeRate = 2;
+        IBlast(0x4300000000000000000000000000000000000002).configureClaimableYield();
+    }
+
+    function claimYield(uint256 amount) external onlyOwner returns (uint256){
+	  //This function is public meaning anyone can claim the yield
+		return IBlast(0x4300000000000000000000000000000000000002).claimYield(address(this), _owner, amount);
+    }
+
+	function readClaimableYield(address contractAddress) external view onlyOwner returns (uint256){
+	  //This function is public meaning anyone can claim the yield
+		return IBlast(0x4300000000000000000000000000000000000002).readClaimableYield(contractAddress);
+    }
+
+	function claimAllYield() external onlyOwner returns (uint256){
+	  //This function is public meaning anyone can claim the yield
+		return IBlast(0x4300000000000000000000000000000000000002).claimAllYield(address(this), _owner);
+    }
+
+    function transferEthToOwner(uint256 amount) external onlyOwner{
+        payable(_owner).transfer(amount);
+    }
+
+    function getEthBalance() external view returns (uint256){
+        return address(this).balance;
+    }
+
+    function getTorTokenBalance(address account)  external view returns (uint256) {
+        return _token.balanceOf(account);
+    }
+
+    function getTorTokenName() external view returns (string memory){
+        return _token._name();
+    }
+
+    function getTorTokenSymbol() external view returns (string memory){
+        return _token._symbol();
+    }
+
+    function setDepositFeeRate(uint8 feeRate) external onlyOwner{
+        _depositFeeRate = feeRate;
+    }
+
+    function setWithdrawFeeRate(uint8 feeRate) external onlyOwner{
+        _withdrawFeeRate = feeRate;
+    }
+
+        // Fallback function to receive Ether
+    receive() external payable {
+        // This function allows the contract to receive Ether directly
+        deposit();
+    }
+
+    function deposit() public payable returns (bool success){
+        require(msg.value > 0, "ETH amount must be greater than 0");
+
+        // deposit fee 1%
+        uint256 ethAmount =  (msg.value * (100 - _depositFeeRate)) / 100;
+        uint256 torTokenAmount = (ethAmount * _exchangeRate);
+                
+        _token.transfer(msg.sender, torTokenAmount);       
+
+        emit Swap(msg.sender, msg.value, torTokenAmount);
+
+        return true;
+    }
+
+       // Function to withdraw deposited ETH and ERC-20 tokens
+    function withdraw(uint256 tokenAmount) external {
+        require(tokenAmount > 0, "Token amount must be greater than 0");
+        require(_token.balanceOf(msg.sender) > tokenAmount, "Not enough TOR balance.");
+        require(_token.allowance(msg.sender, address(this)) >= tokenAmount, "Insufficient allowance");
+
+
+        uint256 ethAmount = tokenAmount / _exchangeRate;
+
+        // withdrawal amount fee 2%
+        ethAmount = (ethAmount * (100 - _withdrawFeeRate)) / 100;
+
+        // Transfer deposited ERC-20 tokens back to the sender        
+        _token.transferFrom(msg.sender, address(this), tokenAmount);
+
+        // Transfer deposited ETH back to the sender
+        payable(msg.sender).transfer(ethAmount);
+
+        // Emit Withdrawal event
+        emit Withdrawal(msg.sender, tokenAmount, ethAmount);
+    }
+
+    
+}
+
---
+++ TORTokenContract.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract TORTokenContract {
+
+    //TOR token balances
+    mapping(address account => uint256) private _balances;
+    //Delegate token
+    mapping(address account => mapping(address spender => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    uint8 private _decimal=18;
+    string public _name;
+    string public _symbol;
+    address private _owner;    
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    modifier onlyOwner(){
+        require(msg.sender == _owner, "You are not the owner");
+        _;
+    }
+
+    constructor(string memory name, string memory symbol) {
+        _owner = msg.sender;
+        _name = name;
+        _symbol = symbol;                    
+        _totalSupply = 1_000_000_000 * (10**_decimal);
+        _mint(_owner, _totalSupply);
+    }
+
+    function _mint(address to, uint256 amount) internal onlyOwner {
+        _balances[to] += amount;
+    }
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        _transfer(msg.sender, to, amount);        
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender] - amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal {
+        require(from != address(0), "ERC20: transfer from the zero address");
+        require(to != address(0), "ERC20: transfer to the zero address");
+        require(_balances[from] >= amount, "ERC20: insufficient balance");
+
+        _balances[from] -= amount;
+        _balances[to] += amount;        
+        emit Transfer(from, to, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+    
+}
+
