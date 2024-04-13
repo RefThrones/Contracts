57822bdb3b566d29a28a506b737756498fbd8e91
inkyu.han <amave99@gmail.com>
2024-04-14 00:40:16 +0900

    fix typo

--- ignition/modules/ReferralThrones.js
+++ ignition/modules/ReferralThrones.js
@@ -1,6 +1,6 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-module.exports = buildModule("ReferalThronesModule", (m) => {
+module.exports = buildModule("ReferralThronesModule", (m) => {
   const OwnerGroupContract = m.contract(
     "OwnerGroupContract", 
     [
