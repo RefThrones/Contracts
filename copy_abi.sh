f0cb8eb0dc5e596357907a91245952d7f400df1a
logan1208 <62975766+logan1208@users.noreply.github.com>
2024-04-26 00:14:32 +0900

    update abi files

---
+++ copy_abi.sh
@@ -0,0 +1,15 @@
+
+echo "cp EthTreasuryContract"
+cp ./artifacts/contracts/EthTreasuryContract.sol/EthTreasuryContract.json ./abi
+echo "cp OwnerGroupContract"
+cp ./artifacts/contracts/OwnerGroupContract.sol/OwnerGroupContract.json ./abi
+echo "cp RefThrone"
+cp ./artifacts/contracts/RefThrone.sol/RefThrone.json ./abi
+echo "cp RefThroneTypes"
+cp ./artifacts/contracts/RefThroneTypes.sol/RefThroneTypes.json ./abi
+echo "cp TORTokenContract"
+cp ./artifacts/contracts/TORTokenContract.sol/TORTokenContract.json ./abi
+echo "cp UserContract"
+cp ./artifacts/contracts/UserContract.sol/UserContract.json ./abi
+echo "cp UserHistory"
+cp ./artifacts/contracts/UserHistory.sol/UserHistory.json ./abi
